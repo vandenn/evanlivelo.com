d17e945512a0820dd785dc9bbeca40937c14d20a
vandenn <evan.livelo@gmail.com>
2025-10-02 01:31:58 +0800

    Minor CSS adjustments and shift of description to About

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className="w-full mt-4">
+    <footer className="w-full">
       <div className="w-4/5 max-w-5xl mx-auto px-8 py-6 max-sm:w-[calc(100%-2rem)] max-sm:mx-4 max-sm:px-4 border-t border-black/10">
         <div className="flex justify-between items-center max-sm:flex-col max-sm:gap-4 max-sm:items-center max-sm:text-center">
           <div>
